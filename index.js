0d959a9058b6a3e3d10749e3cac1d094be281d2e
ItsGeeBee <gracelbrown@hotmail.com>
2022-07-21 11:29:36 +1000

    installed required packages, started on questions array

---
+++ index.js
@@ -0,0 +1,39 @@
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/employee");
+
+
+const questions = {
+    Manager: [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's id?",
+            
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email address?",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number?",
+        },
+        {
+            type: "list",
+            name: "addNew",
+            message: "Do you want to add another employee",
+            choices: ["yes", "no"]
+        }
+    ]
+};
