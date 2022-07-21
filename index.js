e6ae6e969c39b47a119acaa7eb0a130bff819756
ItsGeeBee <gracelbrown@hotmail.com>
2022-07-21 13:05:54 +1000

    Started addNewEmployee function. Worked on planning HTML output

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./lib/employee");
 
-
+const selectEmployee = [
+    {
+        type: "list",
+        name: "selectEmployee",
+        message: "Please choose a role for the employee",
+        choices: ["Manager", "Engineer", "Intern"],
+    }
+];
 const questions = {
     Manager: [
         {
@@ -28,6 +35,63 @@ const questions = {
             type: "input",
             name: "officeNumber",
             message: "What is the manager's office number?",
+        },
+        {
+            type: "list",
+            name: "addNew",
+            message: "Do you want to add another employee",
+            choices: ["yes", "no"]
+        }
+    ],
+    Engineer: [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's id?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineer's email address?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineer's GitHub username?",
+        },
+        {
+            type: "list",
+            name: "addNew",
+            message: "Do you want to add another employee",
+            choices: ["yes", "no"]
+        }
+    ],
+
+    Intern: [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's id?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email address?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school is the intern attending?",
         },
         {
             type: "list",
